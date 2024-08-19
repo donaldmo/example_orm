e7d125661d852a869c0fd61e68d777bece7307f7
Donald Motswiri <domotswiri@gmail.com>
2024-08-19 12:21:35 +0200

    finished RecipeIngredientDO

---
+++ src/main/java/za/co/wethinkcode/persist/orm/RecipeIngredientDO.java
@@ -0,0 +1,20 @@
+package za.co.wethinkcode.persist.orm;
+
+/*
+ * TODO javadoc RecipeIngredientDO
+ */
+public class RecipeIngredientDO {
+    public int productId;
+    public String productName;
+
+    public int ingredientId;
+    public String ingredientName;
+
+    public String ingredientType;
+    public int qty;
+
+    public String units;
+
+    public RecipeIngredientDO() {
+    }
+}
